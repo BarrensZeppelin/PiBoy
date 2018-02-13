0dbf3f53c2e01e7c5c7c6a83c8928eae991a6836
Oskar Haarklou Veileborg <ohv1020@hotmail.com>
2018-02-13 08:47:46 +0100

    Delay clock signal when running on an RPi 3

--- GBLink.cpp
+++ GBLink.cpp
@@ -1,3 +1,5 @@
+#include <unistd.h>
+#include <fstream>
 #include "GBLink.hpp"
 
 void GBLink::initSlave(PinData& pinData) {
@@ -7,6 +9,13 @@ void GBLink::initSlave(PinData& pinData) {
 
 	digitalWrite(pinData.clock, 1);
 	digitalWrite(pinData.write, 0);
+
+	// I found that the Raspberry Pi 3 is a bit too fast,
+	// so the clock signal is delayed a bit to compensate.
+	std::ifstream modelFd("/proc/device-tree/model");
+	std::string model(std::istreambuf_iterator<char>(modelFd), {});
+	modelFd.close();
+	clockDelay = (model.find("Raspberry Pi 3") !== std::string::npos ? 500 : 0);
 }
 
 void GBLink::sendClock(PinData& pinData) {
@@ -20,9 +29,11 @@ void GBLink::sendByte(uint8_t send, PinData& pinData) {
 			digitalWrite(pinData.write, 1);
 		else
 			digitalWrite(pinData.write, 0);
-		
+
 		digitalWrite(pinData.clock, 0);
+		usleep(clockDelay);
 		digitalWrite(pinData.clock, 1);
+		usleep(clockDelay);
 
 		send <<= 1;
 	}
--- GBLink.hpp
+++ GBLink.hpp
@@ -2,9 +2,11 @@
 #include "PinData.hpp"
 
 class GBLink {
+		uint32_t clockDelay;
+
 	public:
 		void initSlave(PinData& pinData);
-		
+
 		void sendClock(PinData& pinData);
 		void sendByte(uint8_t send, PinData& pinData);
 };
